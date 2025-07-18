fa5e4335a498d3eefbb7f3f716def24ac713318b
modithapahan <modithapahan0@gmail.com>
2025-07-18 22:09:41 +0530

    update nav menu section

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,56 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import "../styles/NavMenu.css";
 
 const menuItems = [
-  { label: "Me", icon: "😊", link: "#me" },
-  { label: "Projects", icon: "💼", link: "#projects" },
-  { label: "Skills", icon: "🧠", link: "#skills" },
-  { label: "Contact", icon: "📬", link: "#contact" },
+  { label: "Me", iconClass: <i class="fa-solid fa-user"></i>, link: "#me" },
+  { label: "Projects", iconClass: <i class="fa-solid fa-briefcase"></i>, link: "#projects" },
+  { label: "Skills", iconClass: <i class="fa-solid fa-layer-group"></i>, link: "#skills" },
+  { label: "Contact", iconClass: <i class="fa-solid fa-id-badge"></i>, link: "#contact" },
 ];
 
 function NavMenu() {
-  const searchRef = useRef(null);
-  const [isBlurred, setIsBlurred] = useState(false);
-
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      if (!searchRef.current) return;
-
-      const rect = searchRef.current.getBoundingClientRect();
-      const mouseX = e.clientX;
-      const mouseY = e.clientY;
-
-      const centerX = rect.left + rect.width / 2;
-      const centerY = rect.top + rect.height / 2;
-
-      const distance = Math.hypot(centerX - mouseX, centerY - mouseY);
-
-      // Trigger blur if distance is below threshold
-      setIsBlurred(distance < 150); // you can tweak this
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
 
   return (
     <div className="nav-container">
-      <div
-        ref={searchRef}
-        className={`search-bar ${isBlurred ? "blurred" : ""}`}
-      >
-        {/* <div
-          ref={searchRef}
-          className={`search-bar ${isBlurred ? "blurred" : ""}`}
-        >
-          <input type="text" placeholder="Ask me anything..." />
-          <button>
-            <i class="fa-solid fa-arrow-right"></i>
-          </button>
-        </div> */}
+      <div className="search-bar">
         <input type="text" placeholder="Ask me anything..." />
         <button>
           <i class="fa-solid fa-arrow-right"></i>
@@ -59,7 +21,7 @@ function NavMenu() {
       <div className="menu-buttons">
         {menuItems.map((item, index) => (
           <a href={item.link} key={index} className="menu-button">
-            <div className="icon">{item.icon}</div>
+            <div className="icon">{item.iconClass}</div>
             <div className="label">{item.label}</div>
           </a>
         ))}
